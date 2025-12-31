99308a05e9f9b233fef3dbeaf83ed61951a940f6
Harshini R <143336011+harshinirajesh@users.noreply.github.com>
2025-12-31 22:25:52 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -5,7 +5,7 @@ import {toast} from 'react-toastify'
 
 const DoctorContextProvider = (props) => {
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://clinicease-backend-nxv9.onrender.com"
 
     const [dToken, setDToken] = useState(localStorage.getItem("dToken") ? localStorage.getItem("dToken") : "")
     const [appointments, setAppointments] = useState([])
